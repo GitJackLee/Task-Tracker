39299ea07f402e3bbc8f1b947007036b9afaf513
Jack Anderson <andersonjackwork@gmail.com>
2016-08-07 14:39:53 +0900

    Deleted files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var timer = document.getElementById("timer");
-var start = document.getElementById("start");
-var stop = document.getElementById("stop");
-var reset = document.getElementById("reset");
-var save = document.getElementById("save");
-
-start.addEventListener("click", startClock);
-stop.addEventListener("click", stopClock);
-reset.addEventListener("click", resetClock);
