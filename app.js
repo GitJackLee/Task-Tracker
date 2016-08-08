431fed859f1e2d1530f9c41535a3c293bc36aaef
Jack Anderson <andersonjackwork@gmail.com>
2016-08-08 09:15:13 +0900

    Added the task schema and mongoose.Promise

--- app.js
+++ app.js
@@ -4,13 +4,42 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
 //APP CONFIGURATION
+mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/task_tracker");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var taskSchema = new mongoose.Schema({
+  name: String,
+  timer: Number
+});
+
+var Task = mongoose.model("Task", taskSchema);
+
+// var todo = new Task({
+//   name: "Codewars",
+//   timer: 1470580179289,
+// });
+
+// todo.save(function(err, theTodo){
+//   if(err){
+//     console.log("There was an error: ");
+//     console.log(err);
+//   } else {
+//     console.log(theTodo);
+//   }
+// });
+
 app.get("/", function(req, res){
-  res.render("index");
+  //Get all tasks from DB
+  Task.find({}, function(err, tasks){
+    if(err){
+      console.log(err);
+    } else{
+      res.render("index", {tasks: tasks});
+    }
+  })
 })
 
 app.listen(3000, function(){
